a5c1da3e0bb04a91b3b15d02a8732ddb9563f467
Sixto José Romero Martínez <sixto.jose@gmail.com>
2020-03-15 01:04:52 -0500

    Se organiza las rutas para que apunte a la base de datos que está en heroku

--- src/database.js
+++ src/database.js
@@ -1,10 +1,17 @@
 const mysql = require('mysql');
 
-const mysqlConnection = mysql.createConnection({
+/*const mysqlConnection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '***51x70.j053',
     database: 'caol'
+});*/
+
+const mysqlConnection = mysql.createConnection({
+    host: 'us-cdbr-iron-east-04.cleardb.net',
+    user: 'bd174f69686133',
+    password: '5fa10d1a',
+    database: 'heroku_d5531d8aec95f1a'
 });
 
 mysqlConnection.connect(function (err) {
