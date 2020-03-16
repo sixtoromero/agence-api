ef58a5d638038eade4d4ee22a9e1c594a612f8b6
Sixto José Romero Martínez <sixto.jose@gmail.com>
2020-03-16 02:54:44 -0500

    Se aplican cambios para agregar puerto

--- src/database.js
+++ src/database.js
@@ -1,17 +1,20 @@
 const mysql = require('mysql');
 
-/*
-const mysqlConnection = mysql.createConnection({
+
+/*const mysqlConnection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '***51x70.j053',
     database: 'caol'
-});
-*/
+});*/
+
+
+
 const mysqlConnection = mysql.createConnection({
     host: 'us-cdbr-iron-east-04.cleardb.net',
     user: 'b3c99f7dabe4d1',
     password: 'c469fb22',
+    port: 3306,
     database: 'heroku_14d779aa1e89abe'
 });
 
